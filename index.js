1596815408513ad5bcec5d64e0e532f12619c09a
DaniPop88 <danilla.vargas1923@gmail.com>
2025-08-06 04:32:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const { google } = require("googleapis");
+import express from "express";
+import cors from "cors";
+import { google } from "googleapis";
 
 const app = express();
 app.use(cors());
